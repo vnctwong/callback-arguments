f2340bc1275603426d323de17efdd8ed598c0122
vnctwong <vincentkywong@gmail.com>
2019-02-07 23:35:30 +0000

    refactored with forEach

--- callback-arguments.js
+++ callback-arguments.js
@@ -1,10 +1,28 @@
-// WHAT INDEX IS WALDO FOUND
+// // WHAT INDEX IS WALDO FOUND
+// function findWaldo(arr, found) {
+//   for (var i = 0; i < arr.length; i++) {
+//     if (arr[i] === "Waldo") {
+//       found(i);   // execute callback
+//     }
+//   }
+// }
+
+// function actionWhenFound(i) {
+//   console.log("Found Waldo at index", i + '!');
+// }
+
+// findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
+
+
+
+// REFACTOR FINDWALDO
+
 function findWaldo(arr, found) {
-  for (var i = 0; i < arr.length; i++) {
+  arr.forEach(function findwaldo(arguments, i) {
     if (arr[i] === "Waldo") {
       found(i);   // execute callback
     }
-  }
+  });
 }
 
 function actionWhenFound(i) {
@@ -12,9 +30,3 @@ function actionWhenFound(i) {
 }
 
 findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
-
-
-
-// REFACTOR FINDWALDO
-
-
